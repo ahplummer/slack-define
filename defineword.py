786f07feebe9754232218f82d7026ff9421a99ac
Allen Plummer <ahplummer@gmail.com>
2019-03-18 16:28:30 -0500

    Added special word functionality

--- defineword.py
+++ defineword.py
@@ -6,7 +6,17 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST'])
+def getSpecialDefinition(word):
+    result = "Alas, there is no definition for: " + word
+    with open('specialwords.json') as json_data:
+        data = json.load(json_data)
+        try:
+            result = data["words"][word]
+        except:
+            pass
+    return result
+
+@app.route("/define", methods = ['POST'])
 def define():
     # here we want to get the value of user (i.e. ?user=some-value)
     if request.method == 'POST':
@@ -20,6 +30,15 @@ def define():
         if len(jsonload) > 0:
             for defn in jsonload[0]['shortdef']:
                 return 'One definition for ' + word + ' is: ' + defn + '.'
+
+@app.route("/definespecial", methods = ['POST'])
+def definedi():
+    # here we want to get the value of user (i.e. ?user=some-value)
+    if request.method == 'POST':
+        data = request.form
+        word = data['text']
+        defn = getSpecialDefinition(word)
+        return 'The special definition for ' + word + ' is: ' + defn + '.'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8511)
